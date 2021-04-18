e32fde4b682efe5dcbdb8f554e6b22a62a01ef08
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2021-04-18 12:03:12 -0500

    Added comments

--- LibraryApplication/LibraryApplication/LibraryApplicationForm.cs
+++ LibraryApplication/LibraryApplication/LibraryApplicationForm.cs
@@ -12,8 +12,8 @@ namespace LibraryApplication
 {
     public partial class LibraryApplicationForm : Form
     {
-        Library l = new Library();
-        Dictionary<int, string> isbn = new Dictionary<int, string>();
+        Library l = new Library(); //Create a Library object
+        Dictionary<int, string> isbn = new Dictionary<int, string>(); //Create Dictionary to hold ISBN (key) and title (value)
 
         public LibraryApplicationForm()
         {
@@ -40,30 +40,30 @@ namespace LibraryApplication
 
             if (pop)
             {
-                l.Popular.Add(txtTitle.Text);
-                isbn.Add(int.Parse(txtIsbn.Text), txtTitle.Text);
-                l.Popular.Sort();
+                l.Popular.Add(txtTitle.Text); //Add to Popular list
+                isbn.Add(int.Parse(txtIsbn.Text), txtTitle.Text); //Add to ISBN Dictionary
+                l.Popular.Sort(); //Sort list alphabetically
 
                 for (int i = 0; i < l.Popular.Count; i++)
                 {
-                    if (i > 2)
+                    if (i > 2) //Ensures user won't enter more than 3 popular books
                     {
                         MessageBox.Show("You can only add up to 3 popular books.");
                         throw new ArgumentException();
                     }
                     if (l.Popular[i] != null)
                     {
-                        labels[i].Text = l.Popular[i];
-                        var matchingIsbn = isbn.FirstOrDefault(x => x.Value == l.Popular[i]).Key;
-                        isbnLabels[i].Text = matchingIsbn.ToString();
+                        labels[i].Text = l.Popular[i]; //Populate the label on the GUI
+                        var matchingIsbn = isbn.FirstOrDefault(x => x.Value == l.Popular[i]).Key; //Find the ISBN for the current title
+                        isbnLabels[i].Text = matchingIsbn.ToString(); //Display the ISBN
                     }
                 }
             }
             else
             {
-                l.Books.Add(txtTitle.Text);
-                isbn.Add(int.Parse(txtIsbn.Text), txtTitle.Text);
-                l.Books.Sort();
+                l.Books.Add(txtTitle.Text); //Add to Books list
+                isbn.Add(int.Parse(txtIsbn.Text), txtTitle.Text); //Add to ISBN Dictionary
+                l.Books.Sort(); //Sort list alphabetically
 
                 for (int i = 0; i < l.Books.Count; i++)
                 {
@@ -71,16 +71,16 @@ namespace LibraryApplication
 
                     if (l.Books[i] != null)
                     {
-                        if(r > 8)
+                        if(r > 8) //Ensures user won't enter more than 6 non-popular books
                         {
                             MessageBox.Show("You can only add up to 6 books that aren't popular.");
                             throw new ArgumentException();
                         }
                         else
                         {
-                            labels[r].Text = l.Books[i];
-                            var matchingIsbn = isbn.FirstOrDefault(x => x.Value == l.Books[i]).Key;
-                            isbnLabels[r].Text = matchingIsbn.ToString();
+                            labels[r].Text = l.Books[i]; //Populate the label on the GUI, starting with labels[r] instead of labels[i] because labels[i] would start at labels[0], and indexes 0-2 are popular books only
+                            var matchingIsbn = isbn.FirstOrDefault(x => x.Value == l.Books[i]).Key; //Find the ISBN for the current title
+                            isbnLabels[r].Text = matchingIsbn.ToString(); //Display the ISBN
                         }
                     }
                 }
